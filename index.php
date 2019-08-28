c820939619eab053e9b3db8787870e6f2fcced77
masayukinii1011 <masayukinii1011@gmail.com>
2019-08-28 21:35:59 +0900

    index.php

--- index.php
+++ index.php
@@ -1,16 +1,30 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+    integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
   <title>DatePicker</title>
+  <style>
+    input {
+      width: 0;
+      height: 0;
+      visibility: collapse;
+    }
+  </style>
 </head>
+
 <body>
-  <p>
+  <button>ああああ</button>
   <input type="month">
 </body>
 <script>
-console.log("test")
+  $('button').on('click', function () {
+    $('input').trigger('focus')
+  })
 </script>
+
 </html>
